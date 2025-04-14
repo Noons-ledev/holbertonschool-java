56c6579e68bb5c448edc317ae8c0e28ccaf0782e
Noons-ledev <noah.briet@icloud.com>
2025-04-14 10:57:01 +0200

    Pi & not PI

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String [] args) {
         float number = (float) Math.PI ;
-        System.out.printf("PI Value: %.3f\n", number);
+        System.out.printf("Pi Value: %.3f\n", number);
     }
 }
