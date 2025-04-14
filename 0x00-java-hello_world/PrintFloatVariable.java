a78f92388b48c852fba68ff1c04d014da6ee350c
Noons-ledev <noah.briet@icloud.com>
2025-04-14 10:56:26 +0200

    Casting number as a float (to avoid type conflict)

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String [] args) {
-        double number = Math.PI ;
+        float number = (float) Math.PI ;
         System.out.printf("PI Value: %.3f\n", number);
     }
 }
